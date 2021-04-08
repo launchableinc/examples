bb507ce4eaa3e3c349763defa64064a7ffc59149
Kohsuke Kawaguchi <kkawaguchi@launchableinc.com>
2021-04-08 10:11:01 -0700

    Fixing the assertion

--- gradle/src/test/java/example/MulTest.java
+++ gradle/src/test/java/example/MulTest.java
@@ -8,6 +8,6 @@ import static org.junit.Assert.*;
 public class MulTest {
     @Test
     public void calc() {
-        assertThat(new Mul().calc(8,2), is(16));
+        assertThat(new Mul().calc(8,2), is(17));
     }
 }
