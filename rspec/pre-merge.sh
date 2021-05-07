1ace512778cdaacc75c6c1dcd1c36a0049cce19d
Yoshiori SHOJI <yshoji@launchableinc.com>
2021-05-07 15:57:04 +0900

    Add `set -e` to demo script file

--- rspec/pre-merge.sh
+++ rspec/pre-merge.sh
@@ -2,6 +2,7 @@
 BUILD_NAME=${GITHUB_RUN_ID:=local-$(date +%s)}
 
 set -x
+set -e
 # Verify that Launchable setup is all correct. Useful primarily while you work on integration
 launchable verify
 
@@ -10,13 +11,6 @@ launchable record build --name "$BUILD_NAME" --source ..
 
 # Find 50% of the relevant tests to run for this change
 launchable subset --target 50% --build "$BUILD_NAME" rspec spec > subset.txt
-
-# function record() {
-#   # Record test results
-#   launchable record test --build "$BUILD_NAME" rspec report/
-# }
-
-# trap record EXIT
 
 # Run rspec with the subset of tests
 bundle exec rspec $(cat subset.txt) --format d --format RspecJunitFormatter --out reports/rspec.xml
