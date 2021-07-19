82aff6a1e09f5b7581b6ef252a56dcc61a7b74af
awilkes <awilkes@users.noreply.github.com>
2021-07-19 14:07:39 -0600

    Update pre-merge.sh

--- gradle/pre-merge.sh
+++ gradle/pre-merge.sh
@@ -1,27 +1,37 @@
 #!/bin/bash -e
 BUILD_NAME=${GITHUB_RUN_ID:=local-$(date +%s)}
 
-set -x
-
 echo "# Get Launchable CLI installed. If you can, make it a part of the builder image to speed things up"
+set -x
 pip3 install --user launchable~=1.0 > /dev/null
+set +x
 export PATH=~/.local/bin:$PATH
 
 echo "# Verify that Launchable setup is all correct. Useful primarily while you work on integration"
+set -x
 launchable verify
+set +x
 
 echo "# Tell Launchable about the build you are producing and testing"
+set -x
 launchable record build --name "$BUILD_NAME" --source ..
+set +x
 
 echo "# Find 25% of the relevant tests to run for this change"
+set -x
 launchable subset --target 25% --build "$BUILD_NAME" gradle src/test/java > subset.txt
+set +x
 
 function record() {
   echo "# Record test results"
+  set -x
   launchable record test --build "$BUILD_NAME" gradle build/test-results/test
+  set +x
 }
 
 trap record EXIT
 
 echo "# Run gradle with the subset of tests"
+set -x
 ./gradlew test $(< subset.txt)
+set +x
