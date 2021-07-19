39fb2fbef863049091cbbeb1f8a3af0a165e8027
awilkes <awilkes@users.noreply.github.com>
2021-07-19 15:24:30 -0600

    Update pre-merge.sh

--- gradle/pre-merge.sh
+++ gradle/pre-merge.sh
@@ -2,42 +2,45 @@
 BUILD_NAME=${GITHUB_RUN_ID:=local-$(date +%s)}
 
 echo -e "\n=== BEGIN LAUNCHABLE ==="
-echo -e "\n# Install the Launchable CLI. If you can, install it as part of the builder image to speed things up\n"
-echo "pip3 install --user launchable~=1.0 > /dev/null"
+echo -e "\n# Install the Launchable CLI. If you can, install it as part of the builder image to speed things up"
+echo -e "# Command: pip3 install --user launchable~=1.0 > /dev/null\n"
 
-pip3 install --user launchable~=1.0 > /dev/null
+(set -x; pip3 install --user launchable~=1.0 > /dev/null)
 
 export PATH=~/.local/bin:$PATH
 
-echo -e "\n# Verify that Launchable setup is all correct. Useful primarily while you work on integration\n"
-echo "+ launchable verify"
+echo -e "\n# Verify that Launchable setup is all correct. Useful primarily while you work on integration"
+echo -e "# Command: launchable verify\n"
 
-launchable verify
+(set -x; launchable verify)
 
-echo -e "\n# Tell Launchable about the build you are producing and testing\n"
-echo "+ launchable record build --name \"\$BUILD_NAME\" --source .."
+echo -e "\n# Tell Launchable about the build you are producing and testing"
+echo -e "# Command: launchable record build --name \"\$BUILD_NAME\" --source ..\n"
 
-launchable record build --name "$BUILD_NAME" --source ..
+(set -x; launchable record build --name "$BUILD_NAME" --source ..)
 
-echo -e "\n# Find 25% of the relevant tests to run for this change\n"
-echo "+ launchable subset --target 25% --build \"\$BUILD_NAME\" gradle src/test/java > subset.txt"
+echo -e "\n# Find 25% of the relevant tests to run for this change"
+echo -e "# Command: launchable subset --target 25% --build \"\$BUILD_NAME\" gradle src/test/java > subset.txt\n"
 
-launchable subset --target 25% --build "$BUILD_NAME" gradle src/test/java > subset.txt
+(set -x; launchable subset --target 25% --build "$BUILD_NAME" gradle src/test/java > subset.txt)
 
-echo "+ cat subset.txt"
-cat subset.txt
+echo -e "\n# Inspect the subset file"
+echo -e "# Command: cat subset.txt\n"
+
+(set -x; cat subset.txt)
 
 function record() {
-  echo -e "\n# Record test results\n"
-  echo "+ launchable record tests --build \"\$BUILD_NAME\" gradle build/test-results/test"
+  echo -e "\n# Record test results"
+  echo -e "# Command: launchable record tests --build \"\$BUILD_NAME\" gradle build/test-results/test\n"
   
-  launchable record tests --build "$BUILD_NAME" gradle build/test-results/test
+  (set -x; launchable record tests --build "$BUILD_NAME" gradle build/test-results/test)
   
   echo -e "\n=== END LAUNCHABLE ===\n"
 }
 
 trap record EXIT
 
-echo -e "\n# Run gradle with the subset of tests\n"
-echo "+ ./gradlew test \$(< subset.txt)"
-./gradlew test $(< subset.txt)
+echo -e "\n# Run gradle with the subset of tests"
+echo -e "# Command: ./gradlew test \$(< subset.txt)\n"
+
+(set -x; ./gradlew test $(< subset.txt))
