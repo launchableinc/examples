f858984105afe33c8ac26f4147f11ef937d22567
sachin-c0d3 <101328529+sachin-c0d3@users.noreply.github.com>
2022-05-23 01:01:09 +0530

    Update MulTest.java

--- gradle/src/test/java/example/MulTest.java
+++ gradle/src/test/java/example/MulTest.java
@@ -9,5 +9,11 @@ public class MulTest {
     @Test
     public void calc() {
         assertThat(new Mul().calc(8,2), is(16));
+        assertThat(new Mul().calc(8,3), is(16));
+        assertThat(new Mul().calc(2,2), is(4));
+        assertThat(new Mul().calc(8,2), is(16));
+        assertThat(new Mul().calc(11,5), is(55));
+        assertThat(new Mul().calc(0,5), is(0));
+        assertThat(new Mul().calc(4,8), is(32));
     }
 }
