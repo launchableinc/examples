1b1df45af04f93372fef8f300709842872cfe82d
ninjinkun <ninjin@mac.com>
2022-06-09 21:29:03 +0900

    feature: add a test to test parametric test

---
+++ pytest/tests/test_func5.py
@@ -0,0 +1,11 @@
+from time import sleep
+import pytest
+
+@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
+def test_func5(test_input, expected):
+    assert eval(test_input) == expected
+
+@pytest.mark.parametrize("x", [0, 1])
+@pytest.mark.parametrize("y", [2, 3])
+def test_foo(x, y):
+    sleep(1)
