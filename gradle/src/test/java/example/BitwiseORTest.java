4da4956b30ae08708169889d844a7c0d0d3656ad
sachin-c0d3 <101328529+sachin-c0d3@users.noreply.github.com>
2022-06-12 00:07:35 +0530

    Update BitwiseORTest.java

--- gradle/src/test/java/example/BitwiseORTest.java
+++ gradle/src/test/java/example/BitwiseORTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-public class AddTest {
+public class BitwiseORTest {
     @Test
     public void calc() {
         assertThat(new BitwiseOR().calc(25,100), is(125));
