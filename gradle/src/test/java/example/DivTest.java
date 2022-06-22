5373f995c455bec2c6b564ac58413f5ba96f120d
sachin-c0d3 <101328529+sachin-c0d3@users.noreply.github.com>
2022-06-22 10:39:15 +0530

    Update DivTest.java

--- gradle/src/test/java/example/DivTest.java
+++ gradle/src/test/java/example/DivTest.java
@@ -10,7 +10,17 @@ import org.junit.Ignore;
 public class DivTest {
     @Ignore		
     @Test
-    public void calc() {
+    public void div1() {
+        assertThat(new Div().calc(10,2), is(5));
+        assertThat(new Div().calc(20,4), is(5));
+        assertThat(new Div().calc(100,20), is(5));
+        assertThat(new Div().calc(5,1), is(5));
+        assertThat(new Div().calc(-5,-1), is(5));
+        assertThat(new Div().calc(25,5), is(5));
+    }
+    @Ignore		
+    @Test
+    public void div2() {
         assertThat(new Div().calc(10,2), is(5));
         assertThat(new Div().calc(20,4), is(5));
         assertThat(new Div().calc(100,20), is(5));
@@ -19,7 +29,21 @@ public class DivTest {
         assertThat(new Div().calc(25,5), is(5));
     }
     @Test
-    public void calc2() {
+    public void div3() {
+        assertThat(new Div().calc(100,20), is(5));
+        assertThat(new Div().calc(5,1), is(5));
+        assertThat(new Div().calc(-5,-1), is(5));
+        assertThat(new Div().calc(25,5), is(5));
+    }
+     @Test
+    public void div4() {
+        assertThat(new Div().calc(100,20), is(5));
+        assertThat(new Div().calc(5,1), is(5));
+        assertThat(new Div().calc(-5,-1), is(5));
+        assertThat(new Div().calc(25,5), is(5));
+    }
+     @Test
+    public void div5() {
         assertThat(new Div().calc(100,20), is(5));
         assertThat(new Div().calc(5,1), is(5));
         assertThat(new Div().calc(-5,-1), is(5));
