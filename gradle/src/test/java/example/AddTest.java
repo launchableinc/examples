d7b8c3a480f9f240ee8d331009f3b65be7b85814
sachin-c0d3 <101328529+sachin-c0d3@users.noreply.github.com>
2022-06-22 11:55:06 +0530

    Update AddTest.java

--- gradle/src/test/java/example/AddTest.java
+++ gradle/src/test/java/example/AddTest.java
@@ -10,5 +10,6 @@ public class AddTest {
     public void calc() {
         assertThat(new Add().calc(3,4), is(7));
         assertThat(new Add().calc(14,4), is(18));
+        assertThat(new Add().calc(14,8), is(22));
     }
 }
