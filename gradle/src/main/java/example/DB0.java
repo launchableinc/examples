4bce7792bd4385ecbde156e37833d9bc322b39fd
shibuiwilliam <shibuiyusuke@gmail.com>
2022-10-19 15:17:39 +0900

    fix test

---
+++ gradle/src/main/java/example/DB0.java
@@ -0,0 +1,7 @@
+package example;
+
+public class DB0 {
+    public int calc(int x, int y, int z) {
+        return x+y+z;
+    }
+}
---
+++ gradle/src/test/java/example/DB1Test.java
@@ -0,0 +1,13 @@
+package example;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+public class DB1Test {
+    @Test
+    public void calc() {
+        assertThat(new DB1().calc(3, 4, 5), is(12));
+    }
+}
