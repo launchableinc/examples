374b04991fea4b721ae2e71e7c5fd78d38ad084b
shibuiwilliam <shibuiyusuke@gmail.com>
2023-02-27 12:56:52 +0900

    change perl to prove

---
+++ prove/lib/Example.pm
@@ -0,0 +1,39 @@
+package Example;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Example - It's new $module
+
+=head1 SYNOPSIS
+
+    use Example;
+
+=head1 DESCRIPTION
+
+Example is ...
+
+=head1 LICENSE
+
+Copyright (C) Konboi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Konboi E<lt>ryosuke.yabuki@gmail.comE<gt>
+
+=cut
+
---
+++ prove/lib/Example/Easy.pm
@@ -0,0 +1,13 @@
+package Example::Easy;
+use 5.008001;
+use strict;
+use warnings;
+use utf8;
+
+sub str_concat {
+    my ($a, $b) = @_;
+
+    return $a.$b;
+}
+
+1;
