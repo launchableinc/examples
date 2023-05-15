8e9bd870f1ec5ea21a9f9585fcf2aac7dbc31008
Konboi <ryosuke.yabuki@gmail.com>
2023-05-15 17:21:52 +0900

    fix test format

--- dotnet/ExampleTest.cs
+++ dotnet/ExampleTest.cs
@@ -10,28 +10,28 @@ public class ExampleTest
     [Test]
     public void TestAdd()
     {
-        Assert.AreEqual(5, Example.add(2, 3));
-        Assert.AreEqual(7, Example.add(4, 3));
+        Assert.That(Example.add(2, 3), Is.EqualTo(5));
+        Assert.That(Example.add(4, 3), Is.EqualTo(7));
     }
 
     [Test]
     public void TestSub()
     {
-        Assert.AreEqual(2, Example.sub(4, 2));
-        Assert.AreEqual(2, Example.sub(4, -2)); // 6 is correct
+        Assert.That(Example.sub(4, 2), Is.EqualTo(2));
+        Assert.That(Example.sub(4, -2), Is.EqualTo(2)); // 6 is correct
     }
 
     [Test]
     public void TestMul()
     {
-        Assert.AreEqual(10, Example.mul(2, 5));
-        Assert.AreEqual(10, Example.mul(5, 5)); // 25 is correct
+        Assert.That(Example.mul(2, 5), Is.EqualTo(10));
+        Assert.That(Example.mul(5, 5), Is.EqualTo(10)); // 25 is correct
     }
 
     [Test]
     public void TestDiv()
     {
-        Assert.AreEqual(2, Example.div(10, 5));
-        Assert.AreEqual(0.5, Example.div(5, 10)); // 0 is correct
+        Assert.That(Example.div(10, 5), Is.EqualTo(2));
+        Assert.That(Example.div(5, 10), Is.EqualTo(0.5)); // 0 is correct
     }
 }
