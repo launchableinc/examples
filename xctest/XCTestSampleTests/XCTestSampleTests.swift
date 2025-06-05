1bae373128a7d1afcca8be2fe477003960395d53
Konboi <ryosuke.yabuki@gmail.com>
2025-06-05 17:59:28 +0900

    init XCTest project

---
+++ xctest/XCTestSampleTests/XCTestSampleTests.swift
@@ -0,0 +1,19 @@
+//
+//  XCTestSampleTests.swift
+//  XCTestSampleTests
+//
+//  Created by Ryosuke Yabuki on 2025/06/05.
+//
+
+import XCTest
+
+final class XCTestSampleTests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertEqual(1 + 1, 2)
+    }
+
+    func testMulti() throws {
+        XCTAssertEqual(3 * 4, 12)
+    }
+}
---
+++ xctest/XCTestSampleUITests/XCTestSampleUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  XCTestSampleUITestsLaunchTests.swift
+//  XCTestSampleUITests
+//
+//  Created by Ryosuke Yabuki on 2025/06/05.
+//
+
+import XCTest
+
+final class XCTestSampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
