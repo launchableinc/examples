9d70efbb928188626ce534688be02dc52dbaaf0e
Konboi <ryosuke.yabuki@gmail.com>
2025-06-06 13:39:29 +0900

    add different class

---
+++ xctest/XCTestSampleTests/XCTestSampleTests2.swift
@@ -0,0 +1,14 @@
+//
+//  XCTestSampleTests.swift
+//  XCTestSampleTests
+//
+//  Created by Ryosuke Yabuki on 2025/06/05.
+//
+
+import XCTest
+
+final class XCTestSampleTests2 : XCTestCase {
+    func testMinus() throws {
+        XCTAssertEqual(5 - 3, 1)
+    }
+}
