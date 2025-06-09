82cef787ebc3465d3274b25eba978a9bde723c08
Konboi <ryosuke.yabuki@gmail.com>
2025-06-09 17:17:26 +0900

    add tests

--- vitest/src/components/Hello.test.tsx
+++ vitest/src/components/Hello.test.tsx
@@ -1,14 +1,17 @@
 // src/components/Hello.test.tsx
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import React from 'react'
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 function Hello() {
-  return <h1>Hello, World!</h1>
+  return <h1>Hello, World!</h1>;
 }
 
-test('renders hello message', () => {
-  render(<Hello />)
-  expect(screen.getByText('Hello, World!')).toBeInTheDocument()
-})
+test("renders hello message", () => {
+  render(<Hello />);
+  expect(screen.getByText("Hello, World!")).toBeInTheDocument();
+});
 
+test("error: renders invalid message", () => {
+  render(<Hello />);
+  expect(screen.getByText("Invalid!!")).toBeInTheDocument();
+});
--- vitest/vitest.config.ts
+++ vitest/vitest.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
-    reporters: ['default', 'junit'],
-    outputFile: 'reports/test-results.xml',
-    globals: true,
-    include: ['src/**/*.test.ts?(x)'],
-  },
-})
+    environment: "jsdom",
+    reporters: ["default", "junit"],
 
+    outputFile: "reports/test-results.xml",
+    globals: true,
+    include: ["**/*.test.ts?(x)"],
+  },
+});
