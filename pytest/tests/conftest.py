352746b122db62ba55c799912ab4e5f41f9a1161
Naoto Ono <nono@cloudbees.com>
2025-08-05 13:29:25 +0900

    fix

--- pytest/tests/conftest.py
+++ pytest/tests/conftest.py
@@ -1,6 +1,6 @@
 def pytest_collection_modifyitems(session, config, items):
     for item in items:
         for marker in item.iter_markers():
-            item.user_properties.append(("name", marker.name))
-            item.user_properties.append(("args", str(marker.args)))
-            item.user_properties.append(("kwargs", str(marker.kwargs)))
+            item.user_properties.append(("marker_name", marker.name))
+            item.user_properties.append(("marker_args", str(marker.args)))
+            item.user_properties.append(("marker_kwargs", str(marker.kwargs)))
